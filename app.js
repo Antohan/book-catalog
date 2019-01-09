8002cd3ce339d8b1b214d2fd22b9beafcb8ed31e
Antohan <justanton19@gmail.com>
2019-01-09 09:51:05 +0700

    Added connection to MongoDB.

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphQlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -43,21 +44,49 @@ app.use(
   `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then(events =>
+            events.map(event => ({
+              ...event._doc,
+              _id: event.id
+            }))
+          )
+          .catch(err => {
+            console.log(err);
+          });
       },
-      createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
+      createEvent: args => {
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date,
-        };
-        events.push(event);
-      },
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: event._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      }
     },
-    graphiql: true,
+    graphiql: true
   })
 );
 
-app.listen(9000);
+const mongoConnectUrl = `mongodb://${process.env.MONGO_USER}:${
+  process.env.MONGO_PASSWORD
+}@ds251894.mlab.com:51894/${process.env.MONGO_DB}`;
+
+const url = 'mongodb://admin:admin123@ds251894.mlab.com:51894/book-catalog';
+
+mongoose
+  .connect(mongoConnectUrl)
+  .then(() => {
+    app.listen(9000);
+  })
+  .catch(err => console.log(err));
