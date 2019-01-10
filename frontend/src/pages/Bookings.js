a81c36c36df939a3ab48326d891a71a6c039fd6e
Antohan <justanton19@gmail.com>
2019-01-10 18:30:28 +0700

    Added basic frontend setup with ReactJS.

---
+++ frontend/src/pages/Bookings.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class BookingsPage extends React.Component {
+  render() {
+    return (
+      <h1>Bookings page</h1>
+    );
+  }
+}
+
+export default BookingsPage;
---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class EventsPage extends React.Component {
+  render() {
+    return (
+      <h1>Events Page</h1>
+    );
+  }
+}
+
+export default EventsPage;
